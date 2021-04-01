e05ada1c085ca2e0078a6dadd0f39172d4c07f7c
Lee Jae Hyun <myleje@gmail.com>
2021-04-01 13:40:00 +0900

    .

--- Program.cs
+++ Program.cs
@@ -7,62 +7,85 @@ namespace CSharpProject
         static void Main(string[] args)
         {
             //
-            string userName = "Zack";
-            int userLevel = 20;
+            // string userName = "Zack";
+            // int userLevel = 20;
 
-            //Console.WriteLine("Hello " + userName);
-            //Console.WriteLine($"Hello {userName} !!");
-            Console.WriteLine($"User = {userName}, Level={userLevel}");
+            // //Console.WriteLine("Hello " + userName);
+            // //Console.WriteLine($"Hello {userName} !!");
+            // Console.WriteLine($"User = {userName}, Level={userLevel}");
 
-            int a = 5;
-            int b = 10;
-            int c = a + b;
+            // int a = 5;
+            // int b = 10;
+            // int c = a + b;
 
-            Console.WriteLine($"{a} + {b} = {c}");
+            // Console.WriteLine($"{a} + {b} = {c}");
             
-            int max = int.MaxValue; //+21
-            int min = int.MinValue; //-21
+            // int max = int.MaxValue; //+21
+            // int min = int.MinValue; //-21
 
-            Console.WriteLine($"Integer's Max = {max:#,##0}, Min = {min:#,##0}");
-            Console.WriteLine($"Max = {max.ToString("#,##0")}");
+            // Console.WriteLine($"Integer's Max = {max:#,##0}, Min = {min:#,##0}");
+            // Console.WriteLine($"Max = {max.ToString("#,##0")}");
 
-            int score = 123;
-            Console.WriteLine($"Score : {score:000000}");
-            Console.WriteLine($"Score : {score:######}");
+            // int score = 123;
+            // Console.WriteLine($"Score : {score:000000}");
+            // Console.WriteLine($"Score : {score:######}");
 
-            //사칙연산
-            int _a = 5;
-            int _b = 10;
+            // //사칙연산
+            // int _a = 5;
+            // int _b = 10;
 
-            int _c = _a - _b;
-            //타입캐스팅 (Type casting, 형변환)
-            /*
-                1. 명시적 형변환
-                2. 암시적 형변환
-            */
-            float d = (float)_a / (float)_b;  //명시적 형변환: 5/10 = 0, 5.0 / 10.0 = 0.5;
-            string exp = "User exp = " + 100; //암시적 형변환
+            // int _c = _a - _b;
+            // //타입캐스팅 (Type casting, 형변환)
+            // /*
+            //     1. 명시적 형변환
+            //     2. 암시적 형변환
+            // */
+            // float d = (float)_a / (float)_b;  //명시적 형변환: 5/10 = 0, 5.0 / 10.0 = 0.5;
+            // string exp = "User exp = " + 100; //암시적 형변환
 
-            Console.WriteLine(d);
+            // Console.WriteLine(d);
 
-            //연산 우선순위
-            int x = 5;
-            int y = 4;
-            int z = 2;
+            // //연산 우선순위
+            // int x = 5;
+            // int y = 4;
+            // int z = 2;
 
-            int sum = x + y * z; 
+            // int sum = x + y * z; 
 
-            //float 타입 (실수타입)
+            // //float 타입 (실수타입)
 
-            float damage = 10.0f;       //f
-            double xp = 20000.0d;       //d
-            decimal coin = 1000.0m;     //m
+            // float damage = 10.0f;       //f
+            // double xp = 20000.0d;       //d
+            // decimal coin = 1000.0m;     //m
 
-            double a1 = 1.0d;
-            double b1 = 3.0d;
+            // double a1 = 1.0d;
+            // double b1 = 3.0d;
             
-            Console.WriteLine(a1/b1);
+            // Console.WriteLine(a1/b1);
 
+            // decimal c1 = 1.0m;
+            // decimal d1 = 3.0m;
+
+            // Console.WriteLine(c1/d1);
+            Cal01(2.0f);
+        }
+
+        //함수(Funcition), 메서드(Method)
+        //함수 원형
+        /*
+            반환타입 함수명(매개변수)
+            {
+
+            }
+        */
+        const float PI = 3.14f;
+
+        static void Cal01(float radius)
+        {
+            //원의 면적 계산 : PI * r ^ 2
+            //float radius = 2.0f;
+            double circle = PI * radius * radius; // PI * Math.Pow(radius,2);
+            Console.WriteLine ($"원의 면적={circle}");
         }
     }
 }
